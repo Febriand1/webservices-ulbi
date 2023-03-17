4cbe13dfe9d5e316b8a8c39a3a46e95bca6e47aa
Febriand1 <fdirga63@gmail.com>
2023-03-17 16:48:23 +0700

    latihan

--- controller/coba.go
+++ controller/coba.go
@@ -2,10 +2,17 @@ package controller
 
 import(
 	"github.com/aiteung/musik"
+	cek "github.com/aiteung/presensi"
 	"github.com/gofiber/fiber/v2"
+	"github.com/Febriand1/webservices-ulbi/config"
 )
 
 func Homepage(c *fiber.Ctx) error {
 	ipaddr := musik.GetIPaddress()
 	return c.JSON(ipaddr)
+}
+
+func GetPresensi(c *fiber.Ctx) error {
+     ps := cek.GetPresensiCurrentMonth(config.Ulbimongoconn)
+     return c.JSON(ps)
 }
